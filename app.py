eb732d000ffe00433ecc96c872998bdb00352811
brooklynbowers <151276772+brooklynbowers@users.noreply.github.com>
2024-04-14 17:27:52 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title='ResumeFit', page_icon=':briefcase:', layout='wide
 st.title('Welcome to ResumeFit 👋')
 st.markdown('We\'re here to guide you through the complexities of the job market. By analyzing your resume and the job descriptions of your interest, ResumeFit identifies the key areas where your qualifications match the job requirements and where you might need improvement. Enhance your resume with tailored advice to not just meet but exceed job expectations. Start your journey to a more fulfilling career today!')
 
-image_url = 'https://raw.githubusercontent.com/brooklynbowers/resumeFit/main/path/to/your/image.png'
+image_url = 'https://raw.githubusercontent.com/brooklynbowers/resumeFit/main/job hunt.jpeg'
 st.image(image_url, caption='HR Process - Image courtesy of Pixabay', width=535)
 
 
