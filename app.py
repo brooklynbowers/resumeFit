0546a10b807c6a33c70b90dbda8ccbff8c48290a
brooklynbowers <151276772+brooklynbowers@users.noreply.github.com>
2024-04-14 11:22:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 st.set_page_config(page_title='ResumeFit', page_icon=':briefcase:', layout='wide')
 
 st.title('Welcome to ResumeFit 👋')
-st.markdown('Helping you align your resume with your job aspirations!')
+st.markdown('We're here to guide you through the complexities of the job market. By analyzing your resume and the job descriptions of your interest, ResumeFit identifies the key areas where your qualifications match the job requirements and where you might need improvement. Enhance your resume with tailored advice to not just meet but exceed job expectations. Start your journey to a more fulfilling career today!')
 
 image_url = 'https://pixabay.com/get/g6b8c4bbb6bcd5ee96361669ce1369a05fb61692044596b93efbb4432174e2a7ce06cd87035b23fc49ee8dd8c9cdd256463298c8c55de56bb4f5e7e2fa7a1c7d2_1280.png'
-st.image(image_url, caption='HR Process - Image courtesy of Pixabay', width=700)
+st.image(image_url, caption='HR Process - Image courtesy of Pixabay', width=500)
 
 
 api_key = os.getenv("OPENAI_API_KEY")
